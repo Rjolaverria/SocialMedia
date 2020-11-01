423c8207732c9d4968e69f70b225897a76d22f1b
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-01 00:42:30 -0400

    Routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-function App() {
-  return <div>Hello!</div>;
-}
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { Navbar } from './components';
+import { Container } from 'semantic-ui-react';
+
+const App = () => (
+    <Router>
+        <Container>
+            <Navbar />
+            <Switch>
+                <Route exact path='/'>
+                    <Home />
+                </Route>
+                <Route exact path='/login'>
+                    <Login />
+                </Route>
+                <Route exact path='/register'>
+                    <Register />
+                </Route>
+            </Switch>
+        </Container>
+    </Router>
+);
 
 export default App;
