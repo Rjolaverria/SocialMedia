5b0c59fe2af5a16792de71853806e4e44db7f515
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-04 17:10:02 -0500

    added auth headers

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import {AuthProvider} from './context/authContext'
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:5000',
+});
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('JWT');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
 
 const client = new ApolloClient({
-    uri: 'http://localhost:5000',
+    link:authLink.concat(httpLink),
     cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
     <ApolloProvider client={client} >
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ApolloProvider>
   ,
   document.getElementById('root')
