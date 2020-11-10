58cbce96014f1b1662e204dee6048c53c829d68d
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-10 13:20:43 -0500

    updated typeDefs

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -44,7 +44,7 @@ module.exports = gql`
         register(input: RegisterInput): User!
         login(username: String!, password: String!): User!
         createPost(body: String!): Post!
-        deletePost(postId: String!): String!
+        deletePost(postId: ID!): String!
         addComment(postId: String!, body: String!): Post!
         deleteComment(postId: String!, commentId: String!): Post!
         handleLike(postId: ID!): Post!
