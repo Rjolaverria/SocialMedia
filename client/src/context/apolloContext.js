53062cdf87055dd06a645df6b6ccba1e5dbb1bc1
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-12 11:46:04 -0500

    moved apollo logic

---
+++ client/src/context/apolloContext.js
@@ -0,0 +1,30 @@
+import {
+    ApolloProvider as Provider,
+    ApolloClient,
+    InMemoryCache,
+    createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:5000',
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('JWT');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+export const ApolloProvider = ({ children }) => {
+    return <Provider client={client}>{children}</Provider>;
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 
-import {AuthProvider} from './context/authContext'
-
-const httpLink = createHttpLink({
-    uri: 'http://localhost:5000',
-});
-
-const authLink = setContext((_, {headers}) => {
-  const token = localStorage.getItem('JWT');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  }
-})
-
-const client = new ApolloClient({
-    link:authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-});
+import { ApolloProvider } from './context/apolloContext';
+import { AuthProvider } from './context/authContext';
 
 ReactDOM.render(
-    <ApolloProvider client={client} >
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ApolloProvider>
-  ,
-  document.getElementById('root')
+    <ApolloProvider>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </ApolloProvider>,
+    document.getElementById('root')
 );
