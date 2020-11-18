9dc5648ca25f2f5dee4d53895044907a58f109fc
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-18 15:15:34 -0500

    updated server url

--- client/src/context/apolloContext.js
+++ client/src/context/apolloContext.js
@@ -7,7 +7,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000',
+    uri: 'https://social-media-merng1.herokuapp.com/',
 });
 
 const authLink = setContext((_, { headers }) => {
